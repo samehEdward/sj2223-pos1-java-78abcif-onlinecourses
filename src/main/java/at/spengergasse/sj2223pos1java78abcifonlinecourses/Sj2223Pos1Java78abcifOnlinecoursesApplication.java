09c922613e0456604ceb4492ea5867f038895fc1
samehEdward <edw21629@spengergasse.at>
2022-11-08 18:15:57 +0100

    Hallo

---
+++ src/main/java/at/spengergasse/sj2223pos1java78abcifonlinecourses/Sj2223Pos1Java78abcifOnlinecoursesApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.sj2223pos1java78abcifonlinecourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sj2223Pos1Java78abcifOnlinecoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sj2223Pos1Java78abcifOnlinecoursesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/spengergasse/sj2223pos1java78abcifonlinecourses/Sj2223Pos1Java78abcifOnlinecoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.sj2223pos1java78abcifonlinecourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sj2223Pos1Java78abcifOnlinecoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
